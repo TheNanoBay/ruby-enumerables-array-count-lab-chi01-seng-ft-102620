92fcdd7cab15aae3cf4da5355129bbed195908ae
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-09 02:38:22 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -4,12 +4,20 @@ def count_strings(array)
     if num.class == String
       count += 1 
     end
-    
+  end  
+  
   return count 
   # Return the total number of strings in the provided array using the count enumerable
 end
 
 def count_empty_strings(array)
+  count = 0 
+  array.count do |num|
+    if num == ""
+      count += 1
+    end
+  end
   
+  return count
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
